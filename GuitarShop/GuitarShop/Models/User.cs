531788d6f53ebc894d4cba20c1ca3444cddcac84
mirotoko <mkavrykov@gmail.com>
2025-04-29 23:22:01 +0300

    created user model

---
+++ GuitarShop/GuitarShop/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace GuitarShop.Models
+{
+    public class User
+    {
+        public int Id { get; set; } // Primary key
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
