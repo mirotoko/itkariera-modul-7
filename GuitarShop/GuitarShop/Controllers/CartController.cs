3f766ce2b7d5b17b93882a72c51a1f31f4f436ba
TwentyOneCharacters <stamatovsci@gmail.com>
2025-05-09 18:05:52 +0300

    TO DO: Complete cart functionality rewrite

--- GuitarShop/GuitarShop/Controllers/CartController.cs
+++ GuitarShop/GuitarShop/Controllers/CartController.cs
@@ -18,49 +18,18 @@ public class CartController : Controller
     // View the current user's cart
     public async Task<IActionResult> Index()
     {
-        var user = await _userManager.GetUserAsync(User);
-        var cartItems = _guitarShopContext.Carts
-            .Include(c => c.Guitar)
-            .Where(c => c.UserId == user.Id)
-            .ToList();
-
-        return View(cartItems);
+        return View();
     }
 
     // Add a guitar to the cart
     public async Task<IActionResult> AddToCart(string guitarName)
     {
-        var user = await _userManager.GetUserAsync(User);
-        var existing = _guitarShopContext.Carts.FirstOrDefault(c => c.UserId == user.Id && c.GuitarName == guitarName);
-
-        if (existing != null)
-        {
-            existing.Quantity++;
-        }
-        else
-        {
-            _guitarShopContext.Carts.Add(new Cart
-            {
-                UserId = user.Id,
-                GuitarName = guitarName,
-                Quantity = 1
-            });
-        }
-
-        await _guitarShopContext.SaveChangesAsync();
         return RedirectToAction("Index");
     }
 
     // Remove a guitar from the cart
     public async Task<IActionResult> RemoveFromCart(int id)
     {
-        var cartItem = await _guitarShopContext.Carts.FindAsync(id);
-        if (cartItem != null)
-        {
-            _guitarShopContext.Carts.Remove(cartItem);
-            await _guitarShopContext.SaveChangesAsync();
-        }
-
         return RedirectToAction("Index");
     }
 }
